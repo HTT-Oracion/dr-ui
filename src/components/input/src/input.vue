d32ebe9236981d202c8f068b37067867e5fc5c4b
jinbin <1367100043@qq.com>
2021-07-22 17:50:59 +0800

    input组件:1

--- src/components/input/src/input.vue
+++ src/components/input/src/input.vue
@@ -1,15 +1,29 @@
 <template>
-  <input
-    :type="type"
-    :value="value"
-    :disabled="disabled"
-    v-bind="$attrs"
-    :class="['dr-input', { 'is-disabled': disabled }]"
-    @input="handleInput"
-    @focus="handleFocus"
-    @blur="handleBlur"
-    @change="handleChange"
-  />
+  <div
+    :class="[
+      'dr-input',
+      {
+        'is-disabled': disabled
+      }
+    ]"
+    @mousemove="hovering = true"
+    @mouseleave="hovering = false"
+  >
+    <template>
+      <input
+        class="dr-input__inner"
+        :type="type"
+        :value="value"
+        :disabled="disabled"
+        v-bind="$attrs"
+        @input="handleInput"
+        @focus="handleFocus"
+        @blur="handleBlur"
+        @change="handleChange"
+      />
+      <span class="dr-input__suffix" v-if="getSuffixVisible()"> </span>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -21,16 +35,38 @@ export default {
       default: 'text'
     },
     value: [String, Number],
-    disabled: Boolean
+    disabled: Boolean,
+    clearabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      hovering: false,
+      focused: false
+    }
+  },
+  computed: {
+    nativeInputValue() {
+      return this.value === null || this.value === undefined
+        ? ''
+        : String(this.value)
+    },
+    showClear() {
+      return this.clearabled && !this.disabled && (this.focused || this.hovering) && 
+    }
   },
   methods: {
     handleInput(evt) {
       this.$emit('input', evt.target.value)
     },
     handleFocus(evt) {
+      this.focused = true
       this.$emit('blur', evt.target.value)
     },
     handleBlur(evt) {
+      this.focused = false
       this.$emit('input', evt.target.value)
     },
     handleChange(evt) {
